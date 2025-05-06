91c3c55cf7b707e7dcdd9a3f73176c39796c1cda
Shivani Vijay <shivanivijayy@outlook.com>
2025-05-05 20:18:41 -0400

    Add files via upload

---
+++ artists.js
@@ -0,0 +1,44 @@
+/**
+ * artists.js
+ *
+ * The app's list of Artists
+ */
+
+window.artists = [
+  {
+    artistId: "AID-1",
+    name: "Michael Jackson",
+    urls: [
+      { url: "https://en.wikipedia.org/wiki/Michael_Jackson", name: "Wikipedia" },
+      {
+        url: "https://www.instagram.com/michaeljackson?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==",
+        name: "Instagram"
+      },
+      { url: "https://open.spotify.com/artist/3fMbdgg4jU18AjLCKBhRSm", name: "Spotify" }
+    ]
+  },
+  {
+    artistId: "AID-2",
+    name: "BTS",
+    urls: [
+      { url: "https://en.wikipedia.org/wiki/BTS", name: "Wikipedia" },
+      {
+        url: "https://www.instagram.com/bts.bighitofficial?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==",
+        name: "Instagram"
+      },
+      { url: "https://open.spotify.com/artist/3Nrfpe0tUJi4K4DXYWgMUX", name: "Spotify" }
+    ]
+  },
+  {
+    artistId: "AID-3",
+    name: "Taylor Swift",
+    urls: [
+      { url: "https://en.wikipedia.org/wiki/Taylor_Swift", name: "Wikipedia" },
+      {
+        url: "https://www.instagram.com/taylorswift?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==",
+        name: "Instagram"
+      },
+      { url: "https://open.spotify.com/artist/06HL4z0CvFAxyc27GXpf02", name: "Spotify" }
+    ]
+  }
+];
